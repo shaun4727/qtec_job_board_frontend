0fa2b75d7049aae5670420fa91cf1c697c85b62d
Shaun Hossain <shaun.mononsoft@gmail.com>
2026-03-07 21:47:46 +0600

    updated

--- components/client-components/sections/featured-job/featured-job.tsx
+++ components/client-components/sections/featured-job/featured-job.tsx
@@ -1,3 +1,10 @@
 export const FeaturedJob = () => {
-  return <div>FeaturedJob</div>;
+  return (
+    <div className="px-4 md:px-36 pt-12">
+      {' '}
+      <h1 className="text-2xl font-bold">
+        Featured <span className="text-secondary-text">jobs</span>
+      </h1>
+    </div>
+  );
 };
